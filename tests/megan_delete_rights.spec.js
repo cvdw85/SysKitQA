bb974c00583b38a188202aa5961f5ac11bb4eea0
Cobus van der Walt <cobus.vanderwalt@crosig.hr>
2023-01-18 00:58:36 +0100

    adds double check that adele was not deleted

--- tests/megan_delete_rights.spec.js
+++ tests/megan_delete_rights.spec.js
@@ -19,4 +19,11 @@ test('test', async ({ page }) => {
   await expect(page.locator('#app > div.app-content > div.notification-details > div.notification-details__data > div.notification-actions > div > div.table-body > ul > div > div:nth-child(1) > span')).toHaveText('User Adele Vance failed to be deleted.');
   // According to the error message, Megan sees that she does not have the rights to delete users.
   await expect(page.locator('#app > div.app-content > div.notification-details > div.notification-details__data > div.notification-actions > div > div.table-body > ul > div > div.table-column.status > span')).toHaveText('Error: Insufficient privileges to complete the operation.');
+  // Megan navigates back to the list of users to double check if Adele Vance is still in the list and truly has not been deleted.
+  await page.getByTestId('svg-icon-moveTo').locator('use').click();
+  // Megan makes the assertion by clicking on Adele just to make sure she wasn't deleted inspite of the error message confirmations that she shouldn't have been deleted.
+  await page.getByRole('link', { name: 'Adele Vance' }).click();
+  await page.getByRole('button', { name: 'close' }).click();
+  // Megan asserts that the corresponding name 'Adele Vance' is displaying correctly after attempted deletion.
+  await expect(page.locator('#app > div.app-content > div.user-details > div.user-details__tiles > div.detail-tile.general-info-tile > div.detail-tile__body > div > div > div > div:nth-child(1) > span')).toHaveText('Adele Vance');
 });
